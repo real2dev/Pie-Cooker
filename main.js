b4fefcaf828025ff50b177ea1ee1becaac44d90b
levi2up <115722206+levi2up@users.noreply.github.com>
2023-05-20 11:18:51 -0400

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ var gameData = {
 
 function cookPie() {
   gameData.pie += gameData.piePerClick;
-  document.getElementById("pieCooked").innerHTML = gameData.pie + " Pies Cooked";
+  document.getElementById("pieCooked").innerHTML = format(gameData.pie) + " Pies Cooked";
 }
 
 function buyPiePerClick() {
@@ -16,8 +16,8 @@ function buyPiePerClick() {
     gameData.pie -= gameData.piePerClickCost;
     gameData.piePerClick += 1;
     gameData.piePerClickCost *= 2;
-    document.getElementById("pieCooked").innerHTML = gameData.pie + " Pies Cooked";
-    document.getElementById("perClickUpgrade").innerHTML = "Upgrade Oven (Currently Level " + format(gameData.piePerClick, 'scientific') + ") Cost: " + gameData.piePerClickCost + " Pies";
+    document.getElementById("pieCooked").innerHTML = format(gameData.pie) + " Pies Cooked";
+    document.getElementById("perClickUpgrade").innerHTML = "Upgrade Oven (Currently Level " + format(gameData.piePerClick) + ") Cost: " + format(gameData.piePerClickCost) + " Pies";
   }
 }
 
@@ -25,7 +25,7 @@ var mainGameLoop = window.setInterval(function() {
   var diff = Date.now() - gameData.lastTick;
   gameData.lastTick = Date.now(); // Don't forget to update lastTick.
   gameData.pie += gameData.piePerClick * (diff / 1000); // divide diff by how often (ms) mainGameLoop is ran
-  document.getElementById("pieCooked").innerHTML = gameData.pie + " Pies Cooked";
+  document.getElementById("pieCooked").innerHTML = format(gameData.pie) + " Pies Cooked";
 }, 1000);
 
 var saveGameLoop = window.setInterval(function() {
@@ -37,13 +37,19 @@ if (savegame !== null) {
   gameData = savegame;
 }
 
+function format(number) {
+  var exponent = Math.floor(Math.log10(number));
+  var mantissa = number / Math.pow(10, exponent);
+  if (exponent < 3) return number.toFixed(1);
+  return mantissa.toFixed(2) + "e" + exponent;
+}
+
 function tab(tab) {
   // hide all your tabs, then show the one the user selected.
   document.getElementById("cookPieMenu").style.display = "none";
   document.getElementById("shopMenu").style.display = "none";
   document.getElementById(tab).style.display = "inline-block";
 }
-
 // go to a tab for the first time, so not all show
 tab("cookPieMenu");
 
@@ -51,11 +57,3 @@ if (typeof savegame.pie !== "undefined") gameData.pie = savegame.pie;
 if (typeof savegame.piePerClick !== "undefined") gameData.piePerClick = savegame.piePerClick;
 if (typeof savegame.piePerClickCost !== "undefined") gameData.piePerClickCost = savegame.piePerClickCost;
 if (typeof savegame.lastTick !== "undefined") gameData.lastTick = savegame.lastTick;
-
-function format(number, type) {
-  var exponent = Math.floor(Math.log10(number));
-  var mantissa = number / Math.pow(10, exponent);
-  if (exponent < 3) return number.toFixed(1);
-  if (type === "scientific") return mantissa.toFixed(2) + "e" + exponent;
-  if (type === "engineering") return (Math.pow(10, exponent % 3) * mantissa).toFixed(2) + "e" + (Math.floor(exponent / 3) * 3);
-}
