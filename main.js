aea20d48f1042387933da7f276f0690defd26786
levi2up <115722206+levi2up@users.noreply.github.com>
2023-05-20 07:41:56 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 var gameData = {
   pie: 0,
-  piePerClick: 1
+  piePerClick: 1,
+  piePerClickCost: 10,
 }
 
 function cookPie() {
   gameData.pie += gameData.piePerClick
   document.getElementById("pieCooked").innerHTML = gameData.pie + " Pies Cooked"
+}
+
+function buyPiePerClick() {
+  if (gameData.pie >= gameData.piePerClickCost) {
+    gameData.pie -= gameData.piePerClickCost
+    gameData.piePerClick += 1
+    gameData.piePerClickCost *= 2
+  }
 }
