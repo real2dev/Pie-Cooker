8624747e4f1515e492923d380350178d487ca0d4
levi2up <115722206+levi2up@users.noreply.github.com>
2023-05-19 22:34:28 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,3 +2,8 @@ var gameData = {
   pie: 0,
   piePerClick: 1
 }
+
+function cookPie() {
+  gameData.pie += gameData.piePerClick
+  document.getElementById("pieCooked").innerHTML = gameData.pie + " Pies Cooked"
+}
