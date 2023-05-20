68ea2d17fcec56b45cb835f4f430d18bf1919ced
levi2up <115722206+levi2up@users.noreply.github.com>
2023-05-20 11:14:22 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,52 +1,61 @@
-var saveGame = localStorage.getItem('pieCookerSave')
+var saveGame = localStorage.getItem('pieCookerSave');
 var gameData = {
   pie: 0,
   piePerClick: 1,
   piePerClickCost: 10,
   lastTick: Date.now()
-}
+};
 
 function cookPie() {
-  gameData.pie += gameData.piePerClick
-  document.getElementById("pieCooked").innerHTML = gameData.pie + " Pies Cooked"
+  gameData.pie += gameData.piePerClick;
+  document.getElementById("pieCooked").innerHTML = gameData.pie + " Pies Cooked";
 }
 
 function buyPiePerClick() {
   if (gameData.pie >= gameData.piePerClickCost) {
-    gameData.pie -= gameData.piePerClickCost
-    gameData.piePerClick += 1
-    gameData.piePerClickCost *= 2
-    document.getElementById("pieCooked").innerHTML = gameData.pie + " Pies Cooked"
-    document.getElementById("perClickUpgrade").innerHTML = "Upgrade Oven (Currently Level " + format(gameData.piePerClick, scientific) + ") Cost: " + gameData.piePerClickCost + " Pie"
+    gameData.pie -= gameData.piePerClickCost;
+    gameData.piePerClick += 1;
+    gameData.piePerClickCost *= 2;
+    document.getElementById("pieCooked").innerHTML = gameData.pie + " Pies Cooked";
+    document.getElementById("perClickUpgrade").innerHTML = "Upgrade Oven (Currently Level " + format(gameData.piePerClick, 'scientific') + ") Cost: " + gameData.piePerClickCost + " Pies";
   }
 }
 
 var mainGameLoop = window.setInterval(function() {
-  diff = Date.now() - gameData.lastTick;
-  gameData.lastTick = Date.now() // Don't forget to update lastTick.
-  gameData.pie += gameData.piePerClick * (diff / 1000) // divide diff by how often (ms) mainGameLoop is ran
-  document.getElementById("pieCooked").innerHTML = gameData.pie + " Pies Cooked"
-}, 1000)
+  var diff = Date.now() - gameData.lastTick;
+  gameData.lastTick = Date.now(); // Don't forget to update lastTick.
+  gameData.pie += gameData.piePerClick * (diff / 1000); // divide diff by how often (ms) mainGameLoop is ran
+  document.getElementById("pieCooked").innerHTML = gameData.pie + " Pies Cooked";
+}, 1000);
 
 var saveGameLoop = window.setInterval(function() {
-  localStorage.setItem("pieCookerSave", JSON.stringify(gameData))
-}, 15000)
+  localStorage.setItem("pieCookerSave", JSON.stringify(gameData));
+}, 15000);
 
-var saveGame = JSON.parse(localStorage.getItem("pieCookerSave"))
-if (saveGame !== null) {
-  gameData = savegame
+var savegame = JSON.parse(localStorage.getItem("pieCookerSave"));
+if (savegame !== null) {
+  gameData = savegame;
 }
 
 function tab(tab) {
   // hide all your tabs, then show the one the user selected.
-  document.getElementById("cookPieMenu").style.display = "none"
-  document.getElementById("shopMenu").style.display = "none"
-  document.getElementById(tab).style.display = "inline-block"
+  document.getElementById("cookPieMenu").style.display = "none";
+  document.getElementById("shopMenu").style.display = "none";
+  document.getElementById(tab).style.display = "inline-block";
 }
-// go to a tab for the first time, so not all show
-tab("cookPieMenu")
 
-if (typeof saveGame.pie !== "undefined") gameData.pie = saveGame.pie;
-if (typeof saveGame.piePerClick !== "undefined") gameData.piePerClick = saveGame.piePerClick;
-if (typeof saveGame.piePerClickCost !== "undefined") gameData.piePerClickCost = saveGame.piePerClickCost;
-if (typeof saveGame.lastTick !== "undefined") gameData.lastTick = saveGame.lastTick
+// go to a tab for the first time, so not all show
+tab("cookPieMenu");
+
+if (typeof savegame.pie !== "undefined") gameData.pie = savegame.pie;
+if (typeof savegame.piePerClick !== "undefined") gameData.piePerClick = savegame.piePerClick;
+if (typeof savegame.piePerClickCost !== "undefined") gameData.piePerClickCost = savegame.piePerClickCost;
+if (typeof savegame.lastTick !== "undefined") gameData.lastTick = savegame.lastTick;
+
+function format(number, type) {
+  var exponent = Math.floor(Math.log10(number));
+  var mantissa = number / Math.pow(10, exponent);
+  if (exponent < 3) return number.toFixed(1);
+  if (type === "scientific") return mantissa.toFixed(2) + "e" + exponent;
+  if (type === "engineering") return (Math.pow(10, exponent % 3) * mantissa).toFixed(2) + "e" + (Math.floor(exponent / 3) * 3);
+}
